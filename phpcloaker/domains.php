fd1702be08178f17e1082b409960087343c3eabd
installer <installer@gmail.com>
2024-06-27 11:32:27 +0200

    added keepalive ssh and remoteip apache module tasks

---
+++ phpcloaker/domains.php
@@ -0,0 +1,24 @@
+<?php
+$domains = array(
+    'adsunchained.com',
+    'getunchainedleads.com',
+    'tryunchainedleads.com',
+    'unchainedads.com',
+    'unchainedmarketingleads.com',
+    'unchainedperformancemarketing.com',
+    'useunchainedleads.com',
+    'gounchainedleads.com',
+    'getunchainedleads.com',
+    'unchained1.xyz',
+    'unchained2.xyz',
+    'unchained3.xyz',
+    'unchained4.xyz',
+    'unchained5.xyz',
+    'unchained6.xyz',
+    'unchained7.xyz',
+    'unchained8.xyz',
+    'unchained9.xyz',
+    'unchained10.xyz'
+);
+?>
+
--- phpcloaker/index.php
+++ phpcloaker/index.php
@@ -1,14 +1,43 @@
 <?php
 // Include the log_visit function from the separate file
 include 'log_visit.php';
+include 'domains.php';
+
 require_once("cloaker.class.php");
 //const CLOAK_MODE = "Quick";
 const CLOAK_MODE = "Secure";
-
+define("AMOUNT_ENTRIES", 1000);
+define("AMOUNT_INTERLINK", 50);
 log_visit();
 
-if (cloaker::IsGoogle(CLOAK_MODE)) {
-    header("Location: https://google.com/", true, 301);
+//$domains = array(
+  //         'adsunchained.com',
+    //    'getunchainedleads.com',
+      //  'tryunchainedleads.com',
+        //'unchainedads.com',
+        //'unchainedmarketingleads.com',
+        //'unchainedperformancemarketing.com',
+        //'useunchainedleads.com',
+        //'gounchainedleads.com',
+        //'getunchainedleads.com',
+       // 'unchained1.xyz',
+       // 'unchained2.xyz',
+       // 'unchained3.xyz',
+        //'unchained4.xyz',
+        //'unchained5.xyz',
+        //'unchained6.xyz',
+        //'unchained7.xyz',
+        //'unchained8.xyz',
+        //'unchained9.xyz',
+        //'unchained10.xyz'
+
+//);
+
+
+
+
+if (!cloaker::IsGoogle(CLOAK_MODE)) {
+    header("Location: https://unchain.link", true, 301);
         return;
         }
 
@@ -18,21 +47,58 @@ if (cloaker::IsGoogle(CLOAK_MODE)) {
         curl_setopt($ch, CURLOPT_TIMEOUT, 15);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec( $ch );
-	curl_close( $ch );
+        curl_close( $ch );
 
-	/* --- added --- */
-	$links = "";
-	for ($i=0; $i<10; $i++) {
-		    $links .= '<a href="/' . rand(10,99) . '/' . rand(10000,999999) . '.html">' . rand(10,1000) . '</a><br />' . "\n";
-	}
 
-	$response = str_replace("[[links]]", $links, $response);
-	/* --- added end --- */
-//	echo "printing links variable";
-//	echo $links;
+        //add internal links
+        $links = "";
+
+        // New interlinking embed section
+        for ($i = 0; $i < AMOUNT_INTERLINK; $i++) {
+                $index = rand(0, count($domains) - 1);
+                $target = $domains[$index];
+
+        $links .= sprintf(
+                '<embed src="https://%s/eg/%s.html" type="application/xml" autostart="true" loop="true" width="20" height="20" />' . "\n",
+                $target, RandNum()
+                );
+        $links .= sprintf(
+                '<object data="https://%s/og/%s.html" width="40" height="30" type="text/html">%s</object>' . "\n",
+                $target, RandNum(), RandNum()
+                );
+        $links .= sprintf(
+                '<iframe src="https://%s/ig/%d.html"></iframe>' . "\n",
+                $target, RandNum()
+                );
+        }
+
+
+
+        for ($i=0; $i< AMOUNT_ENTRIES; $i++) {
+        $links .= '<a href="/int/' . rand(10,99) . '/' . rand(10000,999999999) . '.html">' . rand(10,99999) . '</a><br />' . "\n";
+}
+
+        //add external interlinking
+
+
+
+        for ($i=0; $i<9; $i++) {
+        $links .= '<a href="https://' . $domains[rand(0,count($domains)-1)] . '/ext/' . rand(10,99) . '/' . rand(10000,999999999) . '.html">' . rand(10,99999) . '</a><br />' . "\n";
+}
+
+
+
+        $response = str_replace("[[links]]", $links, $response);
+        /* --- added end --- */
+//      echo "printing links variable";
+//      echo $links;
 
         header('Content-length: ' . strlen($response));
 
         echo $response;
+
+function RandNum(){
+    return 100000 + rand(0,89999999);
+}
 
 ?>
